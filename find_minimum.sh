76a6d0040530d060ea70536a32fd80484f8dd4c9
Matt Welch <mattw0308@gmail.com>
2014-04-12 09:52:26 -0700

    Example maze is working, find minimum looks for minimum of robot_movement

--- find_minimum.sh
+++ find_minimum.sh
@@ -3,15 +3,17 @@
 I=1
 GRIPPERS=2
 #COMMAND="./clingo bw_p4 -c maxstep=${I} -c grippers=${GRIPPERS} 1"
-COMMAND="./clingo bw_p3 -c maxstep=${I} -c grippers=${GRIPPERS} 1"
+#COMMAND="./clingo bw_p3 -c maxstep=${I} -c grippers=${GRIPPERS} 1"
+COMMAND="./clingo robot_movement -c maxstep=${I} 1"
+
 echo $COMMAND
 RESULT=$(${COMMAND} | grep "^SATISFIABLE" --color=auto)
 #echo $RESULT
 while [ -z ${RESULT} ]
 do 
     I=$(( $I + 1 ))
-#    echo $I
-    COMMAND="./clingo bw_p3 -c maxstep=${I} -c grippers=${GRIPPERS} 1"
+	#    echo $I
+	COMMAND="./clingo robot_movement -c maxstep=${I} 1"
     echo $COMMAND
     RESULT=$(${COMMAND} | grep "^SATISFIABLE" --color=auto)
 #    echo $RESULT
